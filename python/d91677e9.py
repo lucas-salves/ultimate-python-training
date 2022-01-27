396623cdcf938d5dd7f67190b6e135c7d8e71a21
Lucas Sousa Alves <salves.lucas@outlook.com>
2022-01-27 15:32:25 -0300

    fix d91677e9 training

--- python/d91677e9.py
+++ python/d91677e9.py
@@ -15,4 +15,4 @@ quantity = 3
 itemnumber = 567
 price = 49.65
 myorder = "I want to pay {2} dollars for {0} pieces of item {1}"
-print( myorder.format(quantity, itemnumberm, price) )
+print( myorder.format(quantity, itemnumber, price) )

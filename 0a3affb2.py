6e18319a3f3b81db53736212dcb0fb32dff1e626
Lucas Sousa Alves <salves.lucas@outlook.com>
2022-01-27 08:22:53 -0300

    Add exercise for 0a3affb2 TIL

---
+++ 0a3affb2.py
@@ -0,0 +1,25 @@
+# Variables can even change the type after they have been set.
+
+x = 3
+x = "Sally"
+print(x)
+
+# Specifying the data type of a variable:
+
+x = str(3) # x will be '3'
+y = int(3) # y will be 3
+z = float(3) # z will be 3.0
+
+print(type(x), type(y), type(z))
+
+#Strings can be declared by using single or double quotes.
+
+x = "John Smith"
+## is the same as:
+x = "John Smith"
+
+
+#Variables are case sensitive.
+a = 4
+A = "Sally"
+# A will not overwrite a. This will create two variables.

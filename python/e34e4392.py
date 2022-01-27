819fa324487c530a04021c82194a752d6786e764
Lucas Sousa Alves <salves.lucas@outlook.com>
2022-01-27 08:49:13 -0300

    Add e34e4392 training

---
+++ python/e34e4392.py
@@ -0,0 +1,13 @@
+# Output variables
+
+y = "Python is"
+x = "awesome"
+print(y+x)
+
+# For numbers, the + character works as a mathematical operator.
+# It's not possible to concatenate a string and a number. Python will throw an error: TypeError: unsupported operand type(s) for +: 'int' and 'str'
+
+x = 5
+y = "Lucas"
+print(x + y)
+
